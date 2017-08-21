4ec3e08885af75a2ae9cb3b35b52d4db6d1699c2
Augusto Fraga Giachero <augustofg96@gmail.com>
2017-08-21 15:13:20 -0300

    Remove useless comment

--- tools/db_gen.sh
+++ tools/db_gen.sh
@@ -5,6 +5,5 @@ arg_array=( "S@" )
 count=0
 for name in "$@"; do
 	./food_classifier.py output_${count}.csv $name.csv output_$((count+1)).csv $name
-	#./food_classifier.py
 	count=$((count+1))
 done
