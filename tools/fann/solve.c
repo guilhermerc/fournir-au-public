8f0eda0e260671b30b1664ec333f03f8abfea9c5
Augusto Fraga Giachero <augustofg96@gmail.com>
2017-09-18 09:08:21 -0300

    FANN Neural Network tester
    Now we have a proper tester for neural networks generated with the
    trainer program.

--- tools/fann/solve.c
+++ tools/fann/solve.c
@@ -1,20 +1,72 @@
 #include <stdio.h>
-#define FIXEDFANN
+#include <string.h>
 #include "floatfann.h"
 
-int main()
+int main(int argc, char *argv[])
 {
+	int interactive = 1;
+	if (argc != 2 && argc != 3)
+	{
+		printf("Usage: %s netfile.net [optional --non-interactive]\n", argv[0]);
+		return 1;
+	}
+	
+	if (argc == 3)
+	{
+		if (strcmp(argv[2], "--non-interactive") == 0)
+		{
+			interactive = 0;
+		}
+	}
+	
 	fann_type *calc_out;
-	fann_type input[2];
+	char *netfilename = argv[1];
+	struct fann *ann = fann_create_from_file(netfilename);
+	if (ann == NULL)
+	{
+		printf("Error when loading the neural network file.");
+		return 2;
+	}
+	unsigned int num_outputs = fann_get_num_output(ann);
+	unsigned int num_inputs = fann_get_num_input(ann);
+	unsigned int num_layers = fann_get_num_layers(ann);
+	fann_type *inputs = malloc(num_inputs*sizeof(fann_type));
 
-	struct fann *ann = fann_create_from_file("xor_float.net");
+	if (interactive)
+	{
+		printf("Loaded %s network\n", netfilename);
+		printf("Inputs: %d Outputs: %d Hidden layers: %d\n", num_inputs,
+			   num_outputs, num_layers - 2);
+	}
 
-	input[0] = -1;
-	input[1] = 1;
-	calc_out = fann_run(ann, input);
+	while (1)
+	{
+		int i;
+		float inval;
+		if (interactive) printf("Inputs: ");
+		for (i = 0; i < num_inputs; i++)
+		{
+			if (scanf("%f", &inval) == EOF)
+				break;
 
-	printf("xor test (%f,%f) -> %f\n", input[0], input[1], calc_out[0]);
+			inputs[i] = inval;
+		}
 
+		if (i != num_inputs)
+			break;
+
+		calc_out = fann_run(ann, inputs);
+		if (interactive) printf("Result:\n");
+		
+		for (i = 0; i < num_outputs; i++)
+		{
+			printf("%f ", calc_out[i]);
+		}
+		printf("\n");
+		
+	}
+
+	free(inputs);
 	fann_destroy(ann);
 	return 0;
 }
