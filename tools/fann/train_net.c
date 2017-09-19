05aae3fce3c99af82e1f7fae8e5640eb7ce263aa
Augusto Fraga Giachero <augustofg96@gmail.com>
2017-09-19 10:18:49 -0300

    Save the best NN of all epochs
    In the previous implementation if the desired error was never achieved
    the trainer would keep training the NN until the max number of epochs
    was reached, and would save the NN of the last train. The problem with
    this behavior was that if over trained the NN started to increase the
    mean square error instead decreasing it and the best NN of all epochs
    would be lost.
    
    To solve this problem the trainer makes a copy of the NN in each epoch
    if it produced a smaller error than all previous epochs, so only the
    best of all NNs is saved.

--- tools/fann/train_net.c
+++ tools/fann/train_net.c
@@ -2,13 +2,14 @@
 #include <string.h>
 #include <stdlib.h>
 #include <fann.h>
+#include <stdint.h>
 
-struct fann *create_network(unsigned int inputs, unsigned int hidden_layers, unsigned int neurons_hidden, unsigned int outputs)
+struct fann *create_network(uint32_t inputs, uint32_t hidden_layers, uint32_t neurons_hidden, uint32_t outputs)
 {
-	unsigned int *layers = malloc((hidden_layers + 2)*(sizeof(unsigned int)));
+	uint32_t *layers = malloc((hidden_layers + 2)*(sizeof(uint32_t)));
 	layers[0] = inputs;
 	layers[hidden_layers + 1] = outputs;
-	for (int i = 1; i < (hidden_layers + 1); i++)
+	for (uint32_t i = 1; i < (hidden_layers + 1); i++)
 	{
 		layers[i] = neurons_hidden;
 	}
@@ -24,13 +25,14 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 
-	const unsigned int num_input = atoi(argv[2]);
-	const unsigned int num_output = atoi(argv[5]);
-	const unsigned int num_hidden_layers = atoi(argv[3]);
-	const unsigned int num_neurons_hidden = atoi(argv[4]);
+	const uint32_t num_input = atoi(argv[2]);
+	const uint32_t num_output = atoi(argv[5]);
+	const uint32_t num_hidden_layers = atoi(argv[3]);
+	const uint32_t num_neurons_hidden = atoi(argv[4]);
 	const float desired_error = (const float) atof(argv[8]);
-	const unsigned int max_epochs = atoi(argv[6]);
-	const unsigned int epochs_between_reports = atoi(argv[7]);
+	const uint64_t max_epochs = atoi(argv[6]);
+	const uint64_t epochs_between_reports = atoi(argv[7]);
+	const char *outnetfile = argv[9];
 
 	struct fann *ann = create_network(num_input, num_hidden_layers,
 											num_neurons_hidden, num_output);
@@ -38,12 +40,42 @@ int main(int argc, char *argv[])
 	fann_set_activation_function_hidden(ann, FANN_SIGMOID_SYMMETRIC);
 	fann_set_activation_function_output(ann, FANN_SIGMOID_SYMMETRIC);
 
-    fann_train_on_file(ann, argv[1], max_epochs, epochs_between_reports, desired_error);
+	struct fann_train_data *tdata = fann_read_train_from_file(argv[1]);
 
-    fann_save(ann, argv[9]);
-	printf("Neural network saved to %s\n", argv[9]);
+	float lastmin = 1;
+	struct fann *bestnet = fann_copy(ann);
+	float mse = fann_train_epoch(ann, tdata);
+	uint32_t bitfail = fann_get_bit_fail(ann);
+	uint64_t epoch;
+	printf("Epoch: 1 Error: %.8f Bit fail: %d\n", mse, bitfail);
+	for (epoch = 1; epoch < max_epochs; epoch++)
+	{
+		mse = fann_train_epoch(ann, tdata);
+		if (mse < lastmin)
+		{
+			fann_destroy(bestnet);
+			bestnet = fann_copy(ann);
+			lastmin = mse;
+		}
+		if (mse <= desired_error)
+		{
+			break;
+		}
+		if ((epoch % epochs_between_reports) == 0)
+		{
+			bitfail = fann_get_bit_fail(ann);
+			printf("Epoch: %d Error: %.8f Bit fail: %d\n", epoch, mse, bitfail);
+		}
+	}
 
-    fann_destroy(ann);
+	fann_save(bestnet, outnetfile);
+	printf("Finished after %d epochs\n", epoch);
+	printf("Best error: %.8f Bit fail: %d\n", mse, fann_get_bit_fail(bestnet));
+	printf("Neural network saved to %s\n", outnetfile);
 
-    return 0;
+	fann_destroy_train(tdata);
+	fann_destroy(ann);
+	fann_destroy(bestnet);
+
+	return 0;
 }
