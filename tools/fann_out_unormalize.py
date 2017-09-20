3cc09034e4e490cb7316b7a4e316a5d4209baa17
Guilherme R C <guilherme.riciolic@gmail.com>
2017-09-20 17:14:02 -0300

    Reading from standard input instead of argv

--- tools/fann_out_unormalize.py
+++ tools/fann_out_unormalize.py
@@ -2,9 +2,8 @@
 
 import sys
 
-normalized_number = float(sys.argv[1])
-
 def unormalize_elem(value):
     return ((value + 0.5)*(13000 - 1000)) + 1000
 
-print(str(unormalize_elem(normalized_number)))
+for normalized_number in sys.stdin:
+    print(str(unormalize_elem(float(normalized_number))))
